8b58a577361899ad50175a54d002d3e35feb30e5
Nicky <heishaluo@163.com>
2021-08-15 01:53:45 +0800

    http

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+/**
+ * 使用JSON中间件
+ */
+app.use(express.json());
+
 app.listen(port, ()=>{
   console.log('服务已启动!');
 })
@@ -41,4 +46,26 @@ app.get('/posts/:postId', (resquest, response) => {
 
   // 做出响应
   response.send(posts[0])
+});
+
+/**
+ * 创建内容
+ */
+app.post('/posts', (request, response) => {
+  //获取请求里的数据
+  const { content } = request.body;
+
+  // 设置响应状态码
+  response.status(201);
+
+  // 输出请求头部数据
+  console.log(request.headers['sing-along']);
+
+  //设置响应头部数据
+  response.set('Sing-Along', 'How I wonder what you are')
+
+  // 做出响应
+  response.send({
+    message:`成功创建了内容:${content}`
+  });
 });
