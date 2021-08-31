600f77f132a4b4e62bcc65a6888461f6a4f34d5c
Nicky <heishaluo@163.com>
2021-08-31 16:16:53 +0800

    structure

---
+++ src/app/app.middleware.ts
@@ -0,0 +1,38 @@
+import { Request, Response, NextFunction } from "express";
+import { nextTick } from "process";
+
+/**
+ * 输出请求地址
+ */
+export const requestUrl = (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+ console.log(request.url);
+ next();
+}
+
+/**
+ * 默认异常处理器
+ */
+export const defaultErrorHandler = (
+ error:any,
+ request:Request,
+ response:Response,
+ next:NextFunction
+) => {
+let statusCode: number, message: string;
+
+/**
+ * 处理异常
+ */
+switch(error, message) {
+  default:
+    statusCode = 500;
+    message = '服务器暂时出了点问题~~';
+    break;
+}
+
+response.status(statusCode).send({ message });
+}
---
+++ src/post/post.controller.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from "express";
+import { getPosts } from './post.service';
+
+/**
+ * 内容列表
+ */
+export const index = (
+  request:Request,
+  response: Response,
+  next: NextFunction
+) => {
+  const posts = getPosts();
+response.send(posts);
+};
